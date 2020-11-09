be09f1b5c913232978e95f0d4dc11e04fb5f7920
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-09 15:27:36 +0100

    commit-9

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
-
 let guess
+const results = []
+
+function randomInt(n){
+    return Math.floor(Math.random() * n) + 1
+}
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
-
+    guess = randomInt(MAX_NUMBER)
+    setMessage(`Is it ${guess}?`)
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
-    
+    guess = randomInt(MAX_NUMBER)
+    setMessage(`Is it ${guess}?`)
 })
 
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    // Vad ska hända när man tryckt correct?
+    setMessage('Woho! :)')
+})
 
 function setMessage(msg) {
     document.getElementById('message').innerText = msg
@@ -23,8 +33,8 @@ function start() {
     document.querySelector('main').style.display = 'block'
 
     guess = 0
-
-    // setMessage(`Is it ${guess}?`)
+    
+    setMessage(`Is it ${guess}?`)
 }
 
 document
